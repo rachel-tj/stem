e93b45dfb78112aaba4c3385d847ea490434110f
deadfishh <111475197+deadfishh@users.noreply.github.com>
2022-10-03 15:27:33 -0400

    Add files via upload

--- daisy/script.js
+++ daisy/script.js
@@ -1,21 +1,27 @@
 // it seems that these are the instance varibles
 var components = {
-    numRows : 11,
-    numCols : 11,
+    numRows : 16,
+    numCols : 16,
     factor : 0.7,
-    lives : 3
+    lives : 3,
+    alive : true,
+    completed : 0,
 }
 
 // starts the game!
 function startGame()
 {
     table = document.getElementById('field').appendChild(createTable());
+    setNumbers();
+}
+
+function setNumbers()
+{
     let reverse = false;
     traverse(reverse);
     reverse = true;
     traverse(reverse);
 }
-
 
 
 function cellID(i, j)
@@ -37,12 +43,12 @@ function createTable()
             td = document.createElement('td');
             td.id = cellID(i, j);
             row.appendChild(td);
-            if (i != 0 && j != 0)
+            if (i && j)
             {
                 placeSquares(td);
             }
             changeColors(td, i, j);
-            addCellListeners(td);
+            addCellListeners(td, i, j);
         }
         table.appendChild(row);
     }
@@ -51,28 +57,40 @@ function createTable()
 
 function changeColors(td, i, j)
 {
-    if (i === 0 || j === 0)
+    if (!i|| !j)
     {
         td.style.backgroundColor = 'rgb(0, 0, 0, 0)';
         td.style.border = 'none';
-        if (j === 0)
+        if (!j)
         {
-            td.style.width = '100px';
+            td.style.width = '150px';
             td.style.textAlign= 'right';
             td.style.marginRight = '100px';
         }
         else
         {
             td.style.height = '60px';
-            td.style.textAlign = 'bottom';
+            td.style.textAlign = 'flex';
         }
+    }
+    if (j % 5 === 0 && i)
+    {
+        td.style.borderRight = '2.5px solid black';
+    }
+    if (i % 5 === 0 && j)
+    {
+        td.style.borderBottom = '2.5px solid black';
     }
 }
 
-function addCellListeners(td)
+function addCellListeners(td, i, j)
 {
     td.addEventListener('mousedown', function(event)
     {
+        if (!components.alive)
+        {
+            return;
+        }
         if (event.which === 1)
         {
             if (td.textContent)
@@ -83,14 +101,15 @@ function addCellListeners(td)
             {
                 this.style.backgroundColor = 'mediumpurple';
                 this.clicked = true;
+                testRowBlocks(i);
+                testColBlocks(j);
                 return;
             }
             //this.style.backgroundColor = '#E96982';
             this.style.color = 'crimson';
             components.lives--;
             this.textContent = 'X';
-            this.clicked = true;
-            if (components.lives === 0)
+            if (!components.lives)
             {
                 gameOver();
                 return;
@@ -151,7 +170,7 @@ function traverse(reverse)
             }
             else
             {
-                if (count === 0)
+                if (!count)
                     continue;
                 add += count + ' ';
                 count = 0;
@@ -172,6 +191,68 @@ function traverse(reverse)
         count = 0;
     }
 
+}
+
+function testRowBlocks(row)
+{
+    var i;
+    for (i = 0; i < components.numCols; i++)
+    {
+        var cellid = cellID(row, i);
+        cell = document.getElementById(cellid);
+        if (cell && cell.bomb)
+        {
+            if (!cell.clicked)
+            {
+                return;
+            }
+        }
+    }
+    zerocell = document.getElementById(cellID(row, 0));
+    zerocell.style.color = 'dimgrey';
+    components.completed++;
+    for (i = 1; i < components.numCols; i++)
+    {
+        var cellid = cellID(row, i);
+        cell = document.getElementById(cellid);
+        if (!cell.bomb)
+        {
+            cell.textContent = 'X';
+        }
+    }
+}
+
+function testColBlocks(col)
+{
+    var i;
+    for (i = 0; i < components.numRows; i++)
+    {
+        var cellid = cellID(i, col);
+        cell = document.getElementById(cellid);
+        if (cell && cell.bomb)
+        {
+            if (!cell.clicked)
+            {
+                return;
+            }
+        }
+    }
+    zerocell = document.getElementById(cellID(0, col));
+    zerocell.style.color = 'dimgrey';
+    components.completed++;
+    for (i = 1; i < components.numRows; i++)
+    {
+        var cellid = cellID(i, col);
+        cell = document.getElementById(cellid);
+        if (!cell.bomb)
+        {
+            cell.textContent = 'X';
+        }
+    }
+    if (components.completed === (components.numRows + components.numCols - 2))
+    {
+        gameOver();
+    }
 }
 
 
