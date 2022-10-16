cae033504d454e6aea9fe641dc9a2d5d971d2cf5
dead fish <111475197+deadfishh@users.noreply.github.com>
2022-10-16 12:36:35 -0400

    Update script.js

--- wordle/script.js
+++ wordle/script.js
@@ -7,7 +7,7 @@
 */
 
 const comp = {
-    word: 'bitch',
+    word: 'qanon',
     numTrys : 6,
     numLetters : 5,
     currRow : 0,
@@ -19,7 +19,7 @@ const comp = {
 // starts the game!
 async function startGame()
 {
-    comp.word = await makeWord('words.txt');
+    //comp.word = await makeWord('words.txt');
     var x = document.createTextNode(comp.word)
     document.getElementById('theWord').appendChild(x);
     console.log(comp.word);
