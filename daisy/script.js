503d99527950db96a859e50927d82a4bc2ab2fce
dead fish <111475197+deadfishh@users.noreply.github.com>
2022-10-29 13:51:19 -0400

    Add files via upload

--- daisy/script.js
+++ daisy/script.js
@@ -1,114 +1,48 @@
 /*
 * @author deadfishh
 *
-* this is nonogram
-* i wrote it all except for the grid
-* if you know how to make a grid hmu
-* clicking boxes is fun though!!!
+* this is wordle, which i wrote
+* it is a worse version of the new york times wordle
+* but fuck the new york times
 */
 
-// it seems that these are the instance varibles
-var components = {
-    numRows : 11,
-    numCols : 11,
-    factor : 0.7,
-    lives : 3,
-    alive : true,
-    completed : 0,
+const comp = {
+    word: 'qanon',
+    numTrys : 6,
+    numLetters : 5,
+    currRow : 0,
+    currLetter : 0,
+    over : false,
+    names : [],
 }
 
 // starts the game!
-function startGame()
+async function startGame()
 {
-    startButton();
+    comp.word = await makeWord('words.txt');
+    var x = document.createTextNode(comp.word)
+    document.getElementById('theWord').appendChild(x);
+    console.log(comp.word);
+    document.getElementById('field').appendChild(createTable());
 }
 
-function startButton()
+
+async function makeWord(filename)
 {
-    small = document.getElementById('small');
-    medium = document.getElementById('medium');
-    large = document.getElementById('large');
-
-    small.addEventListener('mouseup', function()
+    const rand = fetch (filename)
+    .then(function(response)
     {
-        depressButton(this);
-        openButton(medium);
-        openButton(large);
-        components.numRows = components.numCols = 6;
-        //doSetUp();
+        return response.text();
     })
-    
-    medium.addEventListener('mouseup', function()
+    .then(function(data)
     {
-        depressButton(this);
-        openButton(small);
-        openButton(large);
-        components.numRows = components.numCols = 11;
-        //doSetUp();
+        var words = data.split('\n');
+        x =  words[Math.floor(Math.random() * words.length)];
+        return x;
     })
-
-    large.addEventListener('mouseup', function()
-    {
-        depressButton(this);
-        openButton(small);
-        openButton(medium);
-        components.numRows = components.numCols = 16;
-        //doSetUp();
-    })
-    
-    easy = document.getElementById('easy');
-    easy.addEventListener('mouseup', function()
-    {
-        depressButton(this);
-        openButton(mid);
-        openButton(difficult);
-        components.factor = 0.7;
-        console.log(components.factor);
-    })
-
-    mid = document.getElementById('mid');
-    mid.addEventListener('mouseup', function()
-    {
-        depressButton(this);
-        openButton(easy);
-        openButton(difficult);
-        components.factor = 0.5;
-        console.log(components.factor);
-    })
-
-    difficult = document.getElementById('difficult');
-    difficult.addEventListener('mouseup', function()
-    {
-        depressButton(this);
-        openButton(easy);
-        openButton(mid);
-        components.factor = 0.3;
-        console.log(components.factor);
-    })
-
-    document.getElementById('go').addEventListener('mouseup', function()
-    {
-        doSetUp();
-    })
-    
+    return rand;
 }
 
-function doSetUp()
-{
-    document.getElementById('field').style.display="block";
-    document.getElementById('sizebuttons').style.display="none";
-    document.getElementById('difficulty').style.display="none";
-    table = document.getElementById('field').appendChild(createTable());
-    setNumbers();
-}
-
-function setNumbers()
-{
-    let reverse = false;
-    traverse(reverse);
-    reverse = true;
-    traverse(reverse);
-}
 
 
 function cellID(i, j)
@@ -116,301 +50,218 @@ function cellID(i, j)
     return 'cell-' + i + '-' + j;
 }
 
-// ???
+async function checkEnglish(filename, string)
+{
+    fetch (filename)
+    .then(function(response)
+    {
+        return response.text();
+    })
+    .then(function(data)
+    {
+        if (data.indexOf(string) != -1)
+        {
+            checkLetter()
+        }
+        else
+        {
+            changeToRed();
+        }
+    })
+}
+
+function changeToRed()
+{
+    for (i = 0; i < comp.currLetter; i++)
+    {
+        var cellid = cellID(comp.currRow, i);
+        cell = document.getElementById(cellid);
+        cell.style.color = 'crimson';
+    }
+}
+
+
+
+
+
 function createTable()
 {
     var table, row, td, i, j;
     table = document.createElement('table');
     
-    for (i=0; i<components.numRows; i++)
+    for (i=0; i<comp.numTrys; i++)
     {
         row = document.createElement('tr');
-        for (j=0; j<components.numCols; j++)
+        for (j=0; j<comp.numLetters; j++)
         {
             td = document.createElement('td');
             td.id = cellID(i, j);
             row.appendChild(td);
-            if (i && j)
-            {
-                placeSquares(td);
-            }
-            changeColors(td, i, j);
-            addCellListeners(td, i, j);
         }
         table.appendChild(row);
     }
+    addKeyListners();
     return table;
 }
 
-function changeColors(td, i, j)
+
+function addKeyListners()
 {
-    if (!i|| !j)
+    const keys = document.querySelectorAll('.key');
+    keys.forEach(key =>
     {
-        td.style.backgroundColor = 'rgb(0, 0, 0, 0)';
-        td.style.border = 'none';
-        if (!j)
+        key.addEventListener('mousedown', function ()
         {
-            td.style.width = '150px';
-            td.style.textAlign= 'right';
-            td.style.marginRight = '100px';
-        }
-        else
-        {
-            td.style.height = '60px';
-            td.style.textAlign = 'flex';
-        }
-    }
-    if (j % 5 === 0 && i)
-    {
-        td.style.borderRight = '2.5px solid black';
-    }
-    if (i % 5 === 0 && j)
-    {
-        td.style.borderBottom = '2.5px solid black';
-    }
-}
-
-function addCellListeners(td, i, j)
-{
-    td.addEventListener('mousedown', function(event)
-    {
-        handleEvent(td, i, j, event);
-    })
-    td.addEventListener('mousemove', function(event)
-    {
-        handleEvent(td, i, j, event);
-    })
-
-    td.addEventListener('mouseup', function(event)
-    {
-        //smth
-    })
-}
-
-function placeSquares(td)
-{
-    number = Math.random();
-    if (number <= components.factor)
-    {
-        td.bomb = true;
-    }
-}
-
-function traverse(reverse)
-{
-    var i, j;
-    let count = 0;
-    for (i = 0; i< components.numRows; i++)
-    {
-        var add = '';
-        for (j = 0; j< components.numCols; j++)
-        {
-            if (reverse)
+            if (key.id === 'enter')
             {
-                var cellid = cellID(j, i);
+                handleEnter();
+            }
+            else if (key.id === 'del')
+            {
+                handleDelete();
+            }
+            else if (comp.currLetter >= comp.numLetters || comp.currRow >= comp.numTrys)
+            {
+                return;
             }
             else
             {
-                var cellid = cellID(i, j);
+                var cellid = cellID(comp.currRow, comp.currLetter++);
+                cell = document.getElementById(cellid);
+                if (!comp.over && comp.currLetter <= comp.numLetters)
+                {
+                    cell.textContent = key.id;
+                }
             }
-            cell = document.getElementById(cellid);
-            if (cell.bomb)
+        });
+    });
+
+    addEventListener('keydown', function(event)
+    {
+        body = document.getElementById('body');
+        num = event.key.charCodeAt();
+        if (num === 66 && comp.currLetter)
+        {
+            handleDelete();
+            return;
+        }
+        test: if (num >=97 && num <= 122 && !comp.over)
+        {
+            if (comp.currLetter >= comp.numLetters || comp.currRow >= comp.numTrys)
             {
-                //cell.style.backgroundColor = 'yellow';
+                break test;
+            }
+            var cellid = cellID(comp.currRow, comp.currLetter++);
+            cell = document.getElementById(cellid);
+            cell.textContent = event.key;
+        }
+        if (num === 69)
+        {
+            handleEnter();
+        }
+    });
+}
+
+function checkLetter()
+{
+    var i;
+    var count = 0;
+    for (i = 0; i < 5; i++)
+    {
+        var cellid = cellID(comp.currRow, i)
+        var cell = document.getElementById(cellid);
+        var letter = cell.textContent;
+        var place = comp.word.indexOf(letter);
+        var keyletter = document.getElementById(letter);
+        cell.style.animation = 'fadeIn 3s'
+        if (place != -1)
+        {
+            if (comp.word.charAt(i) === letter)
+            {
+                cell.style.background = 'mediumseagreen';
+                keyletter.style.background = 'mediumseagreen';
                 count++;
             }
             else
             {
-                if (!count)
-                    continue;
-                add += count + ' ';
-                count = 0;
+                cell.style.background = 'gold';
+                if (keyletter.style.background != 'mediumseagreen')
+                {
+                    keyletter.style.background = 'gold';
+                }
             }
-        }
-        if (count)
-            add += count;
-        if (!add && i && j)
-            add = '0';
-        if (reverse)
-        {
-            testRowBlocks(i);
-            zerocell = document.getElementById(cellID(0, i));
-            zerocell.textContent = add
         }
         else
         {
-            testColBlocks(i);
-            zerocell = document.getElementById(cellID(i, 0));
-            zerocell.textContent = add
-        }
-        count = 0;
-    }
-
-}
-
-function testRowBlocks(row)
-{
-    var i;
-    for (i = 0; i < components.numCols; i++)
-    {
-        var cellid = cellID(row, i);
-        cell = document.getElementById(cellid);
-        if (cell && cell.bomb)
-        {
-            if (!cell.clicked)
-            {
-                return;
-            }
+            cell.style.background = 'lightgrey'
+            keyletter.style.background = 'grey';
         }
     }
-    zerocell = document.getElementById(cellID(row, 0));
-    zerocell.style.color = 'dimgrey';
-    components.completed++;
-    for (i = 1; i < components.numCols; i++)
-    {
-        var cellid = cellID(row, i);
-        cell = document.getElementById(cellid);
-        if (!cell.bomb)
-        {
-            cell.textContent = 'X';
-            cell.clicked = true;
-        }
-    }
-}
-
-function testColBlocks(col)
-{
-    var i;
-    for (i = 0; i < components.numRows; i++)
-    {
-        var cellid = cellID(i, col);
-        cell = document.getElementById(cellid);
-        if (cell && cell.bomb)
-        {
-            if (!cell.clicked)
-            {
-                return;
-            }
-        }
-    }
-    zerocell = document.getElementById(cellID(0, col));
-    zerocell.style.color = 'dimgrey';
-    components.completed++;
-    for (i = 1; i < components.numRows; i++)
-    {
-        var cellid = cellID(i, col);
-        cell = document.getElementById(cellid);
-        if (!cell.bomb)
-        {
-            cell.textContent = 'X';
-            cell.clicked = true;
-        }
-    }
-    if (components.completed === (components.numRows + components.numCols))
-    {
-        gameOver();
-    }
-}
-
-
-function gameOver()
-{
-    components.alive = false;
-    if (components.lives)
+    if (count === comp.numLetters)
     {
         document.getElementById('won').style.display="block";
-        document.getElementById('overbuttons').style.display="block";
+        document.getElementById('over').style.display="block";
+        comp.over = true;
     }
-    else
+    comp.currLetter = 0;
+    comp.currRow++;
+    if (comp.currRow === comp.numTrys && !comp.over)
     {
-        document.getElementById('lost').style.display="block";
-        document.getElementById('overbuttons').style.display="block";
+        comp.over = true;
+        document.getElementById('lost').style.display="block";   
+        document.getElementById('over').style.display="block";
     }
-   
 }
 
-function depressButton(button)
+function handleEnter()
 {
-    button.style.boxShadow = 'none';
-    button.style.height = '40px';
-    button.style.width = '140px';
+    if (comp.currLetter < comp.numLetters)
+    {
+        return;
+    }
+    var string = '';
+    for (i = 0; i < comp.numLetters; i++)
+    {
+        var cellid = cellID(comp.currRow, i)
+        var cell = document.getElementById(cellid);
+        var letter = cell.textContent;
+        string += letter
+    }
+   checkEnglish('words.txt', string) 
 }
 
-function openButton(button)
-{
-    button.style.boxShadow = '5px 10px rgba(0, 0, 0, 0.4)';
-    button.style.height = '50px';
-    button.style.width = '150px';
-}
 
-
-function handleEvent(td, i, j, event)
+function handleDelete()
 {
-    td.oncontextmenu = function()
-        { 
-            return false; 
-        };
-        if (!components.alive || !i || !j || td.clicked)
+    if (!comp.currLetter)
+    {
+        return;
+    }
+    var cellid = cellID(comp.currRow, --comp.currLetter);
+    cell = document.getElementById(cellid);
+    cell.textContent = '';
+    if (comp.currLetter === comp.numLetters - 1)
+    {
+        for (i = 0; i < comp.numLetters; i++)
         {
-            return;
+            var cellid = cellID(comp.currRow, i);
+            cell = document.getElementById(cellid);
+            cell.style.color = 'black';
         }
-        if (event.which === 1)
-        {
-            if (td.textContent)
-            {
-                return;
-            }
-            if (td.bomb)
-            {
-                td.style.backgroundColor = 'mediumpurple';
-                td.clicked = true;
-                testRowBlocks(i);
-                testColBlocks(j);
-                return;
-            }
-            td.style.color = 'crimson';
-            components.lives--;
-            td.textContent = 'X';
-            cell.clicked = true;
-            if (!components.lives)
-            {
-                gameOver();
-                return;
-            }
-        }
-        else if (event.which === 3 && !td.clicked)
-        {
-            if (!td.flagged)
-            {
-                td.flagged = true;
-                td.textContent = 'X';
-            }
-            else
-            {
-                td.flagged = false;
-                td.textContent = '';
-            }
-        }
+    }
 }
 
 
 
 
-function newGame()
+
+// the game is over and all of the cells are dead :)
+function gameOver()
 {
-    components.alive = true;
-    components.lives = 3;
-    components.completed = 0;
-    document.getElementById('won').style.display="none";
-    document.getElementById('lost').style.display="none";
-    document.getElementById('overbuttons').style.display="none";
-    //clear();
-    table.remove();
-    table = document.getElementById('field').appendChild(createTable());
-    setNumbers();
+    comp.over = true;
+    document.getElementById('over').style.display="block";
+    
 }
-
-
 
 // reload the winodw
 function reload()
@@ -421,9 +272,6 @@ function reload()
 // loads the game i guess
 window.addEventListener('load', function()
 {
-    document.getElementById('lost').style.display="none";
-    var all = document.getElementById('field');
-        all.onselectstart = () => { return false; }
+    document.getElementById('over').style.display="none";
     startGame();
 });
-
