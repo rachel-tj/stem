b90c7c90c48f1222c43556167f925f508597fdc9
dead fish <111475197+deadfishh@users.noreply.github.com>
2022-10-31 17:04:36 -0400

    Add files via upload

--- scripty.js
+++ scripty.js
@@ -8,4 +8,20 @@ function scrollFunction(id)
       behavior: 'smooth',
       inline: 'start'
     });
-}
+  }
+
+  function crollFunction(id)
+  {
+    var node = document.getElementById(id);
+    var yourHeight = '100px';
+    // scroll to your element
+    node.scrollIntoView(true);
+    // now account for fixed header
+    var scrolledY = window.scrollY;
+    if(scrolledY)
+    {
+      window.scroll(0, scrolledY - yourHeight);
+    }
+  }
+    
+
