289a3ea59d8a6045c3bfcdad3285078c2f74e78c
deadfish <rachel.tjarksen@gmail.com>
2023-02-07 09:37:06 -0500

    table

--- tasks/script.js
+++ tasks/script.js
@@ -24,20 +24,32 @@ function addRow()
     td1 = document.createElement("td")
     td2 = document.createElement("td")
     tdcheck = document.createElement("td")
+    trash = document.createElement("td")
     tdcheck.className = "tdcheck"
+    trash.className = "trash"
+    trash.textContent = "⌫"
     td1.contentEditable = true
     td2.contentEditable = true
-    tdcheck.addEventListener('mousedown', function(event)
+    td1.spellcheck = false;
+    td2.spellcheck =  false;
+    tdcheck.addEventListener('mousedown', function()
     {
         parr = this.parentElement;
         changedColor = parr.style.backgroundColor == "thistle" ? "transparent" : "thistle";
         this.textContent = parr.style.backgroundColor == "thistle" ? "" : "✓"
+        trash.style.backgroundColor = "rgb(250, 250, 250)"
         parr.style.backgroundColor = changedColor;
 
     });
+    trash.addEventListener('mouseup', function()
+    {
+        parr = this.parentElement;
+        table[0].removeChild(parr)
+    })
     extraRow.appendChild(td1)
     extraRow.appendChild(td2)
     extraRow.appendChild(tdcheck)
+    extraRow.appendChild(trash)
     console.log(table)
     table[0].appendChild(extraRow)
 }
