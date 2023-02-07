94257b07d237b545c5a49fffdfb835b77491b6f6
deadfish <rachel.tjarksen@gmail.com>
2023-02-06 23:39:40 -0500

    GUI is for loosers

--- tasks/script.js
+++ tasks/script.js
@@ -7,6 +7,7 @@
 */
 
 
+
 // starts the game!
 function addSection()
 {
@@ -15,3 +16,30 @@ function addSection()
     var opened = window.open("");
     opened.document.write("<html><head><title>MyTitle</title></head><body>test</body></html>");
 }
+
+function addRow()
+{
+    extraRow = document.createElement("tr")
+    table = document.getElementsByTagName("table")
+    td1 = document.createElement("td")
+    td2 = document.createElement("td")
+    tdcheck = document.createElement("td")
+    tdcheck.className = "tdcheck"
+    td1.contentEditable = true
+    td2.contentEditable = true
+    tdcheck.addEventListener('mousedown', function(event)
+    {
+        parr = this.parentElement;
+        changedColor = parr.style.backgroundColor == "thistle" ? "transparent" : "thistle";
+        this.textContent = parr.style.backgroundColor == "thistle" ? "" : "✓"
+        parr.style.backgroundColor = changedColor;
+
+    });
+    extraRow.appendChild(td1)
+    extraRow.appendChild(td2)
+    extraRow.appendChild(tdcheck)
+    console.log(table)
+    table[0].appendChild(extraRow)
+}
+
+
